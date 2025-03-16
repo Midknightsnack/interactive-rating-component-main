848877381a73eb18c692bf3ae3d3823eb6296d7f
Bradley Diep <bdippin12@csu.fullerton.edu>
2025-03-16 15:15:34 -0700

    project complete

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// const btnEl = document.querySelector('.rating-button');
-
-// btnEl.addEventListener('click', ()=> {
-//     btnEl.classList.add('special');
-// });
-
 const button_list = document.querySelectorAll('.rating-button');
+const final_rating = document.getElementById('user-rating');
 
 button_list.forEach(button => {
     button.addEventListener('click', () => {
+        // ? means that in case the first click doesn't exist, it won't give an error and allow us to continue
+        // https://www.youtube.com/watch?v=zh1xus05Kl8&ab_channel=ByteGrad
         document.querySelector('.rating-button-clicked')?.classList.remove('rating-button-clicked');
         button.classList.add('rating-button-clicked');
+
+        final_rating.innerHTML = button.innerHTML;
+        // console.log(button.innerHTML);
     }); 
 });
 
-// const buttons = document.querySelectorAll('.rating-button'); 
- 
-// buttons.forEach(button => { 
-//     button.addEventListener('click', function() { 
-//         buttons.forEach(btn => btn.classList.remove('special')); // Remove active from all 
-//         this.classList.add('special'); // Add active to clicked button 
-//     }); 
-// }); 
+const submit_btn = document.querySelector('.submit-button')
+const rating = document.querySelector('.first-section')
+const rating_after = document.querySelector('.thank-you-state')
+
+rating_after.classList.add('hide');
+
+submit_btn.addEventListener('click', () => {
+    rating.classList.add('hide');
+    rating_after.classList.add('appear');
+});
